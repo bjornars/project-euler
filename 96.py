715c96c7c142857b73b128ff7f7b7aac9a8cdfd2
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-07-23 22:43:52 +0200

    optimized 96

--- 96.py
+++ 96.py
@@ -2,52 +2,48 @@ import itertools
 
 class Thing(object):
     def __init__(self, getter):
-        self.c = [ [] for x in range(9) ]
+        self.c = [[False]*10 for x in range(9) ]
         self.getter = getter
 
     def get(self, x, y):
         return self.c[self.getter(x,y)]
 
-    def __len__(self):
-        return sum(map(len, self.c))
-
-    def dump(self):
-        return sum([sum(x) for x in self.c])
-
 def print_grid(grid):
     print '*' * 10
     for x in grid: print x
 
-def solve(row, col, box, grid, x, y):
-    if x == 9 and y == 8:
-        #print_grid(grid)
+class AbortException():
+    pass
+
+def solve(row, col, box, grid, nr):
+    while nr != 81:
+        x = nr % 9
+        y = nr / 9
+        if grid[x][y] == 0:
+            break
+        nr += 1
+
+    if nr == 81:
+        # print_grid(grid)
         global total
         total += grid[0][0] * 100 + grid[0][1] * 10 + grid[0][2]
-        return
-
-    if x == 9:
-        return solve(row, col, box, grid, 0, y+1)
-
-    if grid[x][y] != 0:
-        return solve(row, col, box, grid, x + 1, y)
+        raise AbortException()
 
     for g in range(1, 10):
-        skip = False
-        for e in box, col, row:
-            if g in e.get(x,y):
-                skip = True
-        if skip:
+        if row.get(x,y)[g] or box.get(x,y)[g] or col.get(x,y)[g]:
             continue
 
-        #print 'trying %s for %sx%s' % (g,  x, y)
-        for e in box, col, row:
-            e.get(x,y).append(g)
+        box.get(x,y)[g] = True
+        col.get(x,y)[g] = True
+        row.get(x,y)[g] = True
 
         grid[x][y] = g
-        solve(row, col, box, grid, x + 1, y)
+        solve(row, col, box, grid, nr + 1)
         grid[x][y] = 0
-        for e in box, col, row:
-            e.get(x,y).remove(g)
+
+        box.get(x,y)[g] = False
+        row.get(x,y)[g] = False
+        col.get(x,y)[g] = False
 
 def start_solve(grid):
     print_grid(grid)
@@ -57,15 +53,18 @@ def start_solve(grid):
     for x,y in itertools.product(range(9), range(9)):
         if grid[x][y] > 0:
             for e in boxs, rows, cols:
-                e.get(x, y).append(grid[x][y])
-    solve(rows, cols, boxs, grid, 0, 0)
+                e.get(x, y)[grid[x][y]] = True
+    solve(rows, cols, boxs, grid, 0)
 
 data = open("96.txt").readlines()
 grids = [data[x+1:x+10] for x in range(0, len(data), 10)]
 
 total = 0
-for grid in grids[:5]:
+for grid in grids:
     grid = [map(int, x.strip()) for x in grid]
-    start_solve(grid)
+    try:
+        start_solve(grid)
+    except AbortException:
+        pass
 
 print total
