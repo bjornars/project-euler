f1555d9cf419dc63aa85bd8e2d89c320b4f7d7b2
Bjørnar Snoksrud <bjornar@snoksrud.no>
2012-07-25 13:29:32 +0200

    27

---
+++ 27.py
@@ -0,0 +1,22 @@
+import itertools
+import lib
+
+# n^2 + an + b
+def num_euler(a, b):
+    for n in itertools.count():
+        if not lib.is_prime(n*n + a*n + b):
+            return n
+
+if __name__ == '__main__':
+    assert num_euler(1, 41) == 40
+    assert num_euler(-79, 1601) == 80
+
+    max_coef = None
+    max_num = -1
+    for a, b in itertools.product(range(-999, 1000), range(-999, 1000)):
+        n = num_euler(a,b)
+        if n > max_num:
+            max_num = n
+            max_coef = (a,b)
+
+    print max_coef
--- lib.py
+++ lib.py
@@ -18,17 +18,17 @@ def triangular(p):
 
 def num_divisors(p):
     num = 0
-    square_root = int(math.ceil(math.sqrt(p))) 
+    square_root = int(math.ceil(math.sqrt(p)))
     for x in xrange(1, square_root):
         if p % x == 0:
             num += 2
     if square_root * square_root == p:
         num += 1
-    return num    
+    return num
 
 def divisors(p):
     div = set()
-    square_root = int(math.ceil(math.sqrt(p))) 
+    square_root = int(math.ceil(math.sqrt(p)))
     for x in xrange(1, square_root+1):
         if p % x == 0:
             div.add(p/x)
@@ -54,11 +54,34 @@ def get_primes(limit):
         for x in range(sieve+sieve, limit, sieve):
             numbers[x] = False
 
+_primes = set()
+_non_primes = set()
+_prime_limit = 1000000
+def is_prime(x):
+    if len(_primes) == 0:
+        _primes.update(get_primes(_prime_limit))
+    if x in _primes:
+        return True
+    if x in _non_primes or x < _prime_limit:
+        return False
+
+    if x % 3 == 0 or x % 2 == 0:
+        _non_primes.add(x)
+        return False
+
+    for d in range(6, int(math.sqrt(x)), 6):
+        if x % (d+1) == 0 or x % (d-1) == 0:
+            _non_primes.add(x)
+            return False
+
+    _primes.add(x)
+    return True
+
 def gcd(a, b):
     if a > b:
         a, b = b, a
     while b != 0:
-        a, b = b, a % b 
+        a, b = b, a % b
     return a
 
 def lcd(a, b):
